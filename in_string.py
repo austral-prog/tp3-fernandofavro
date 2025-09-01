695cf6c267a41bf2a0f6a663800c618668617959
fernandofavro <fjfavrovelo@mail.austral.edu.ar>
2025-09-01 20:20:20 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,7 +1,5 @@
 def check_vowels():
-    nombre = input("Ingrese un nombre: ")
-    nombre = nombre.lower()  # pasar todo a minúscula
-
-    for vocal in "aeiou":
-        print(f"Contiene {vocal}: {vocal in nombre}")
+    nombre = "Matias"
+    nombre = nombre.lower()
+    return {v: v in nombre for v in "aeiou"}
 check_vowels()
