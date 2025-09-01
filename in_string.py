20ea625f4e061d51e9d5b1cad2571224ce1662b7
fernandofavro <fjfavrovelo@mail.austral.edu.ar>
2025-09-01 20:44:23 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,9 +1,5 @@
 def check_vowels():
     nombre = input("Ingrese un nombre: ")
     nombre = nombre.lower()
-
     for vocal in "aeiou":
         print(f"Contiene {vocal}: {vocal in nombre}")
-
-
-check_vowels()
--- slice_simple.py
+++ slice_simple.py
@@ -1,13 +1,7 @@
 def slice_simple():
     texto = "Awesome"
     texto = texto.lower()
-
     print(texto[0:3])
-
     medio = len(texto) // 2
     print(texto[medio-1:medio+2])
-
     print(texto[0:4] + texto[-3:])
-
-
-slice_simple()
